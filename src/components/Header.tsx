75a7c237e5cddc9053c47ea5977a7282ea46f0a0
NikolayFrontEnd <nikolaydon2@mail.ru>
2025-09-11 15:22:06 +0300

    added function to switch between pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,14 @@
 import style from './Header.module.css';
 import logo from '../assets/LogoGreen.png';
+import { useState } from 'react';
 export const Header = () => {
-    
+
+    type PageType = 0 | 1 | 2;
+    const [page, setpage] = useState<PageType>(0);
+    const PageChange = (num:PageType) =>{
+    setpage(num);
+}
+
     return (
         <>
         <div className={style.header}>
@@ -12,9 +19,9 @@ export const Header = () => {
 
 <div className = {style.header__menu}>
 
-    <div className = {style.header__menu__item__active}>Customers</div>
-    <div className = {style.header__menu__item}>Scheduled Membership Cancellation</div>
-    <div className = {style.header__menu__item}>Executed Membership Cancelattion</div>
+    <div onClick = {()=>PageChange(0)} className = { page === 0 ? style.header__menu__item__active : style.header__menu__item }>Customers</div>
+    <div onClick = {()=>PageChange(1)} className = { page === 1 ? style.header__menu__item__active :  style.header__menu__item}>Scheduled Membership Cancellation</div>
+    <div onClick = {()=>PageChange(2)} className = { page === 2 ? style.header__menu__item__active : style.header__menu__item}>Executed Membership Cancelattion</div>
 
 </div>
 
